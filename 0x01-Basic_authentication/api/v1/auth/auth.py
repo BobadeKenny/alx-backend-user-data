7f8479eabefe2ca6c33968b5c57289139c772e00
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-05-30 22:42:52 +0100

    validATE REquests

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth"""
         if path is None or excluded_paths is None or excluded_paths == []:
