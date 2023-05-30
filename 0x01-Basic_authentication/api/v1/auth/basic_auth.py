92d7437985d22e2857cb49c50f5973dbf0cdaca3
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-05-30 23:05:45 +0100

    base64 auth decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,8 +6,7 @@ from .auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class"""
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """extract_base64_authorization_header"""
         if (
             authorization_header is None
@@ -16,3 +15,17 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """decode_base64_authorization_header"""
+        if (
+            base64_authorization_header is None
+            or type(base64_authorization_header) is not str
+        ):
+            return None
+        try:
+            return base64_authorization_header.decode("utf-8")
+        except Exception:
+            return None
