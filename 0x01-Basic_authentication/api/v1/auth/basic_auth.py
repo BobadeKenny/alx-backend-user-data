55e5bfbc758d9fef1548a0c4f94cfe7b3eec14f3
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-05-30 22:59:36 +0100

    base64 auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,10 +8,10 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """extract_base64_authorization_header"""
-        if (
-            authorization_header is None
-            or type(authorization_header) is not str
-            or authorization_header[:6] != "Basic "
-        ):
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
