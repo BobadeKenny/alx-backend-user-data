57beef19b9a24f3803796ace49f533b9b8b69ab1
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-06-08 23:48:24 +0100

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound
-from sqlalchemy.orm.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -31,33 +29,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Save the user to the database"""
+        """Method that saves a new user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """Takes in arbitrary keyword arguments and returns the first row
-        found in the users table as filtered by the method’s input arguments.
-        """
-        all_users = self._session.query(User)
-        for k, v in kwargs.items():
-            if k not in User.__dict__:
-                raise InvalidRequestError
-            for usr in all_users:
-                if getattr(usr, k) == v:
-                    return usr
-        raise NoResultFound
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """Locates the user to update, then will update the user’s attributes
-        as passed in the method’s arguments then commit changes to the database
-        """
-        user = self.find_user_by(id=user_id)
-        for k, v in kwargs.items():
-            if not hasattr(user, k):
-                raise ValueError
-            setattr(user, k, v)
-        self._session.commit()
-        return None
