75a1b20d7ed7ee5be49486ea075263a1a0c98815
Kehinde Bobade <kehinde.bobade@mpharma.com>
2023-06-08 23:41:43 +0100

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -38,14 +39,14 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """Takes in arbitrary keyword arguments and returns the first row
-        found in the users table as filtered by the method’s input
+        found in the users table as filtered by the method’s input arguments.
         """
         if not kwargs:
             raise InvalidRequestError
         if not all(hasattr(User, k) for k in kwargs):
             raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
-        if not user:
+        if user is None:
             raise NoResultFound
         return user
 
